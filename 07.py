d5cd54bbd50f51dd2845daeada470d69063ff04a
Thomas Steck <thomas.steck@ingka.com>
2021-12-07 13:35:33 +0100

    cache rules the world

--- 07.py
+++ 07.py
@@ -2,6 +2,7 @@
 with open("input7-1.txt", "r") as f:
     data = f.read()
 
+import time
 
 numbers = [int(num) for num in data.split(",")]
 mn, mx = min(numbers), max(numbers)
@@ -19,6 +20,11 @@ for mv in range(mn, mx):
 
 print(f"Result 1= {total_cost}, min-num={minimum_num}")
 
+t0 = time.perf_counter()
+
+from functools import cache
+
+@cache
 def sum_prevs(n):
     t = 0
     while n >= 1:
@@ -30,15 +36,11 @@ def sum_prevs(n):
 numbers = [int(num) for num in data.split(",")]
 mn, mx = min(numbers), max(numbers)
 
-print("Len of numbers=", len(numbers))
-
 for mv in range(mn, mx):
-    print("Testing mv=", mv)
     cost = 0
     for num in numbers:
         cost += sum_prevs(abs((num)-mv))
     if mv == mn:
-        print(f"Move={mv}, Min={mn}")
         total_cost = cost
     if cost < total_cost:
         print(f"Cost={cost} is less than total cost={total_cost} for number={mv}")
@@ -47,9 +49,5 @@ for mv in range(mn, mx):
 
 print(f"Result 2= {total_cost}, min-num={minimum_num}")
 
-# 11 
-# 1 1
-# 2 2
-# 3 3
-# 4 4
-# 
+t1 = time.perf_counter()
+print(t1-t0)
