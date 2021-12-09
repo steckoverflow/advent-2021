0cbe6ef600c6cc99624a2f6783df53418bbd2112
Thomas Steck <thomas.steck@ingka.com>
2021-12-09 11:01:12 +0100

    extra comments

--- 09.py
+++ 09.py
@@ -8,22 +8,22 @@ m = [v for v in dt.split("\n") if len(v) > 0]
 for j, r in enumerate(m):
     for i, c in enumerate(r):
         #print(j, i, c) # check up, down, left, right (skipping edge cases)
-        if not j == 0:
+        if not j == 0: # UP
             u = int(m[j-1][i])
             if u < int(c):
                 continue
         
-        if not j == len(m) - 1:
+        if not j == len(m) - 1: # DOWN
             b = int(m[j+1][i])
             if b < int(c):
                 continue
 
-        if not i == 0:
+        if not i == 0: #LEFT
             l = int(m[j][i-1])
             if l < int(c):
                 continue
         
-        if not i == len(r) - 1:
+        if not i == len(r) - 1: # RIGHT
             rt = int(m[j][i+1])
             if rt < int(c):
                 continue
